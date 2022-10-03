64263d51f4bf6f3bf9c60f4eacde54ff13ae3ce7
Varsha <varshashaheen2003@gmail.com>
2022-10-03 13:26:46 +0530

    new programs added

---
+++ 15.pattern3.c
@@ -0,0 +1,20 @@
+/*to print
+ *    if n=3
+ *    1
+ *    23
+ *    456 */
+
+#include <stdio.h>
+
+int main(){
+    int n,i,j,count=1;
+    printf("Enter the number of rows:\n");
+    scanf("%d",&n);
+    for(i=1;i<=n;i++){
+        for(j=0;j<i;j++){
+            printf("%d",count);
+            count++;
+        }
+        printf("\n");
+    }
+}
--- main.cpp
+++ main.cpp
@@ -1,18 +1,19 @@
 /*to print
  *    if n=3
  *    1
- *    22
- *    333 */
+ *    23
+ *    456 */
 
 #include <stdio.h>
 
 int main(){
-    int n,i,j;
+    int n,i,j,count=1;
    printf("Enter the number of rows:\n");
    scanf("%d",&n);
    for(i=1;i<=n;i++){
        for(j=0;j<i;j++){
-           printf("%d",i);
+           printf("%d",count);
+           count++;
        }
        printf("\n");
    }
