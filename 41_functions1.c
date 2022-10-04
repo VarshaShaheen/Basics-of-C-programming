f1f8c28904883d6ed644f516adbed2242fb34528
Varsha <varshashaheen2003@gmail.com>
2022-10-04 18:31:56 +0530

    new programs added

---
+++ 41_functions1.c
@@ -0,0 +1,18 @@
+//function to factorial of a number
+
+#include <stdio.h>
+
+int factorial(int n){
+    if(n==0 || n==1){
+        return(1);
+    }
+    else{
+        return(n*factorial(n-1));
+    }
+}
+int main(){
+    int n;
+    printf("Enter the number:\n");
+    scanf("%d",&n);
+    printf("%d",factorial(n));
+}
